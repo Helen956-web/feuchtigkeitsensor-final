a16fea61a3a02ab0dbb06c5e4f6cc000b4b8147c
Helen956-web <helen.mueller@thg-hn.de>
2024-10-09 10:14:54 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,12 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     let Feuchtigkeit = 0
+    pins.analogWritePin(AnalogPin.P1, 1023)
+    basic.showNumber(Feuchtigkeit, 150)
     if (Feuchtigkeit <= 8) {
-        basic.showNumber(Feuchtigkeit, 150)
         basic.setLedColor(0x00ff00)
     } else if (Feuchtigkeit > 400) {
-        basic.showNumber(Feuchtigkeit, 150)
         basic.setLedColor(0xff0000)
     } else {
-        basic.showNumber(Feuchtigkeit, 150)
         basic.setLedColor(0x0000ff)
     }
 })
